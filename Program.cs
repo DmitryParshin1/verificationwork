36783548fc5c22e508781aaf3d5492bc964b3935
DmitryParshin <dima.rs@mail.ru>
2022-11-27 12:15:28 +0300

    добавлена программа

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿string[] Text(int a)
+{
+    string[] result = new string[a];
+    for(int i = 0; i<a; i++)
+    {
+        Console.Write($"Введите {i} элемент: ");
+        result[i]= Console.ReadLine();
+    }
+    return result;
+}
+
+int Razmer()
+{
+    Console.WriteLine(" Введите кол-во элементов ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+string[] Program(string[] array)
+{
+int a = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            a++;                       
+        }
+    }
+    string[] result = new string[a];
+    a = -1;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+           a++;
+           result[a]= array[i];                       
+        }
+    }
+    return result;
+}
+
+
+void Print(string[] array)
+{
+    Console.WriteLine(string.Join(' ', array));
+}
+
+Print(Program(Text(Razmer())));
